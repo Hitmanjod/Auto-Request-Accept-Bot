5f60e577b838ac34763a81fdb0f6fe93446bb7d4
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-10-29 09:29:06 +0545

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,8 +5,8 @@ class Config:
     API_HASH = getenv("API_HASH", "f0f35dbb5b0081cdc8d3c9d5383c4628")
     BOT_TOKEN = getenv("BOT_TOKEN", "6134077876:AAHeX0SvR7PMgamOMyaQGnBDQAmqpDYOp-k")
     FSUB = getenv("FSUB", "Sujan_BotZ")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    CHID = int(getenv("CHID", "-1001861445521"))
+    SUDO = list(map(int, getenv("SUDO", "5123039648").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sujanch9993:sujanch9993@cluster0.yhmdrl9.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
