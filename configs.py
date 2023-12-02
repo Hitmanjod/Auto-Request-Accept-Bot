f6ba6e48dd28cb08d335bee08fa4a5a8ffd9a9a3
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2023-12-02 18:03:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "25502576"))
     API_HASH = getenv("API_HASH", "f0f35dbb5b0081cdc8d3c9d5383c4628")
     BOT_TOKEN = getenv("BOT_TOKEN", "6517618497:AAHZjQz0dRmEkgyY5I7j5q7vFW5SaFAXI2s")
-    FSUB = getenv("FSUB", "Sujan_BotZ")
+    FSUB = getenv("FSUB", "iMediaUniverse")
     CHID = int(getenv("CHID", "-1001861445521"))
     SUDO = list(map(int, getenv("SUDO", "5123039648").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sujanch9993:sujanch9993@cluster0.yhmdrl9.mongodb.net/?retryWrites=true&w=majority")
